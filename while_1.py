154fbc2015da51cf6955f2df25e829f422432921
malekzahedi <mohamadzahedi1375@gmail.com>
2023-03-09 12:41:47 +0330

    give meaningful names to files

---
+++ while_1.py
@@ -0,0 +1,12 @@
+while True:
+    physic_number = input("Type Number please type a valid integer be joone maderet: ")
+
+    try:
+        physic_number = int(physic_number)
+    except ValueError:
+        print('your input is not in correct format')
+    except NameError:
+        print('your code has name error')
+    else:
+        print(physic_number * 1.2)
+        break
