e7d734e9863042796f1fcc6726c7af6a8ad5ef0a
Sogand N <snoroozeydian@gmail.com>
2023-03-19 18:02:41 +0330

    !

--- practices/PracticeNo9-2.py
+++ practices/PracticeNo9-2.py
@@ -16,9 +16,11 @@ print(r)
 
 # راه دوم
 
-sorce_list = [5,3,9,23,64,15]
-x = -float('inf')
-for i in sorce_list:
-    if x < i:
-        x = i
-print(x)
+def the_biggest_number(*args):
+    x = -float('inf')
+    for i in args:
+        if x < i:
+            x = i
+    return x
+r = the_biggest_number(2,3,6,13,95,12,1,4)
+print(r)
